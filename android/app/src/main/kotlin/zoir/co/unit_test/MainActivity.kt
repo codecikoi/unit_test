0633fde7d0a73367726da44695bf4575d2952742
Zoir I <wowdobryy@gmail.com>
2022-11-26 20:37:35 +0600

    created sipmle unit test & structure the 3 AAA - Arrange, Act, Assert

---
+++ android/app/src/main/kotlin/zoir/co/unit_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package zoir.co.unit_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/calculator_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unit_test/calculator.dart';
+
+void main() {
+  test('calculator return 5 when 1+4', () {
+/// structure the 3 AAA - Arrange , Act, Assert
+
+
+    // Arrange (Setup) -> create calculator object
+    final calculator = Calculator();
+
+    // Act (Action) -> collect the result which you want to test
+    final result = calculator.add(1, 4);
+
+    // Assert (Result) -> compare the result against and expected value
+    expect(
+      result,
+      5,
+      reason: 'it should be exactly 5',
+    );
+  });
+}
