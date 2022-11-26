29c0f815ac8ed162fe907b4d6bdb7fa9f4f5eb49
Zoir I <wowdobryy@gmail.com>
2022-11-26 23:40:05 +0600

    refactored unit test. Used  setUp property

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -2,12 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_test/calculator.dart';
 
 void main() {
+  late Calculator calculator;
+  setUp(() => calculator = Calculator());
+
   group('add', () {
     test('calculator return 5 when 1+4', () {
       /// structure the 3 AAA - Arrange , Act, Assert
 
       // Arrange (Setup) -> create calculator object
-      final calculator = Calculator();
+      // final calculator = Calculator();
 
       // Act (Action) -> collect the result which you want to test
       final result = calculator.add(1, 4);
@@ -27,7 +30,7 @@ void main() {
 
     /// TypeMatcher
     test('calculator returns double number (5.0) 5 when 1+4', () {
-      final calculator = Calculator();
+      // final calculator = Calculator();
 
       final result = calculator.add(1, 4);
 
@@ -40,7 +43,7 @@ void main() {
 
   group('subtract', () {
     test('subtracting 6-1 = 5', () {
-      final calculator = Calculator();
+      // final calculator = Calculator();
       final result = calculator.subtract(6, 1);
 
       expect(result, 5);
@@ -49,7 +52,7 @@ void main() {
 
   group('multiply', () {
     test('multiplying 2 * 5 = 10', () {
-      final calculator = Calculator();
+      // final calculator = Calculator();
       final result = calculator.multiply(2, 5);
 
       expect(result, 10);
@@ -58,26 +61,26 @@ void main() {
 
   group('divide', () {
     test('dividing 10 / 2 = 5', () {
-      final calculator = Calculator();
+      // final calculator = Calculator();
       final result = calculator.divide(10, 2);
 
       expect(result, 5);
     });
     test('calculator throws an ArgumentError when going to divide by zero', () {
-      final calculator = Calculator();
+      // final calculator = Calculator();
       expect(() => calculator.divide(10, 0), throwsArgumentError);
     });
   });
 
   group('powerOfTwo', () {
-    final calculator = Calculator();
+    // final calculator = Calculator();
     test('calculator returns 16 when input is 4', () async {
       expect(await calculator.powerOfTwo(4), 16);
     });
   });
 
   group('fibonacci sequence', () {
-    final calculator = Calculator();
+    // final calculator = Calculator();
     test('calculator returns [0, 1, 1, 2, 3, 5, 8, 13, 21]', () {
       expect(
         calculator.fibonacciSequence(),
