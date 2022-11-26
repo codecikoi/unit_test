087f1090155330cd062a8c52a9d691904a25ac06
Zoir I <wowdobryy@gmail.com>
2022-11-26 23:36:45 +0600

    some kind of unit tests || also for Future, and Stream

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -15,4 +15,16 @@ class Calculator {
     if (b == 0) throw ArgumentError('Cannot be divided by 0');
     return a / b;
   }
+
+  Future<double> powerOfTwo(double a) => Future.delayed(
+        Duration(seconds: 1),
+        () => a * a,
+      );
+
+  Stream<int> fibonacciSequence() => Stream.periodic(
+        Duration(seconds: 1),
+        (count) => _fibonacciValues[count],
+      );
+
+  final List<int> _fibonacciValues = [0, 1, 1, 2, 3, 5, 8, 13, 21];
 }
--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -2,37 +2,89 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_test/calculator.dart';
 
 void main() {
-  test('calculator return 5 when 1+4', () {
-    /// structure the 3 AAA - Arrange , Act, Assert
+  group('add', () {
+    test('calculator return 5 when 1+4', () {
+      /// structure the 3 AAA - Arrange , Act, Assert
 
-    // Arrange (Setup) -> create calculator object
-    final calculator = Calculator();
+      // Arrange (Setup) -> create calculator object
+      final calculator = Calculator();
 
-    // Act (Action) -> collect the result which you want to test
-    final result = calculator.add(1, 4);
+      // Act (Action) -> collect the result which you want to test
+      final result = calculator.add(1, 4);
 
-    // Assert (Result) -> compare the result against and expected value
-    expect(
-      result,
-      5,
-      reason: 'it should be exactly 5',
-    );
+      // Assert (Result) -> compare the result against and expected value
+      expect(
+        result,
+        5,
+        reason: 'it should be exactly 5',
+      );
+    });
+
+    /// method matcher of test
+    test('calculator creates non null object', () {
+      expect(Calculator(), isNotNull);
+    });
+
+    /// TypeMatcher
+    test('calculator returns double number (5.0) 5 when 1+4', () {
+      final calculator = Calculator();
+
+      final result = calculator.add(1, 4);
+
+      expect(
+        result,
+        isA<double>(),
+      );
+    });
   });
 
-  /// method matcher of test
-  test('calculator creates non null object', () {
-    expect(Calculator(), isNotNull);
+  group('subtract', () {
+    test('subtracting 6-1 = 5', () {
+      final calculator = Calculator();
+      final result = calculator.subtract(6, 1);
+
+      expect(result, 5);
+    });
   });
 
-  /// TypeMatcher
-  test('calculator returns double number (5.0) 5 when 1+4', () {
+  group('multiply', () {
+    test('multiplying 2 * 5 = 10', () {
+      final calculator = Calculator();
+      final result = calculator.multiply(2, 5);
+
+      expect(result, 10);
+    });
+  });
+
+  group('divide', () {
+    test('dividing 10 / 2 = 5', () {
+      final calculator = Calculator();
+      final result = calculator.divide(10, 2);
+
+      expect(result, 5);
+    });
+    test('calculator throws an ArgumentError when going to divide by zero', () {
+      final calculator = Calculator();
+      expect(() => calculator.divide(10, 0), throwsArgumentError);
+    });
+  });
+
+  group('powerOfTwo', () {
     final calculator = Calculator();
+    test('calculator returns 16 when input is 4', () async {
+      expect(await calculator.powerOfTwo(4), 16);
+    });
+  });
 
-    final result = calculator.add(1, 4);
-
-    expect(
-      result,
-      isA<double>(),
-    );
+  group('fibonacci sequence', () {
+    final calculator = Calculator();
+    test('calculator returns [0, 1, 1, 2, 3, 5, 8, 13, 21]', () {
+      expect(
+        calculator.fibonacciSequence(),
+        emitsInOrder(
+          [0, 1, 1, 2, 3, 5, 8, 13, 21],
+        ),
+      );
+    });
   });
 }
